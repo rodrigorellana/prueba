9ba544d0f8c522fd93583a21271c6189f15775da
RODRIGOI7\rodrigo <rodrigo.orellana.1982@gmail.com>
2017-07-17 09:12:10 -0400

    table

--- js/main.js
+++ js/main.js
@@ -1,5 +1,28 @@
 
 var app = angular.module('mainApp', ['ui.bootstrap.datetimepicker', 'ui.dateTimeInput']);
 app.controller('mainController', function ($scope) {
+    (function onLoad() {
+    
+        $scope.testArray = [
+            {col1:'1',col2:'xxxxxxxxxxxxx',col3:'xxxxxxxxxxxxx',col4:'xxxxxxxxxxxxx',col5:'xxxxxxxxxxxxx',col6:'200000'},
+            {col1:'2',col2:'xxxxxxxxxxxxx',col3:'xxxxxxxxxxxxx',col4:'xxxxxxxxxxxxx',col5:'xxxxxxxxxxxxx',col6:'100000'},
+            {col1:'3',col2:'xxxxxxxxxxxxx',col3:'xxxxxxxxxxxxx',col4:'xxxxxxxxxxxxx',col5:'xxxxxxxxxxxxx',col6:'50000'},
+            {col1:'4',col2:'xxxxxxxxxxxxx',col3:'xxxxxxxxxxxxx',col4:'xxxxxxxxxxxxx',col5:'xxxxxxxxxxxxx',col6:'200000'},
+            {col1:'5',col2:'xxxxxxxxxxxxx',col3:'xxxxxxxxxxxxx',col4:'xxxxxxxxxxxxx',col5:'xxxxxxxxxxxxx',col6:'100000'},
+            {col1:'6',col2:'xxxxxxxxxxxxx',col3:'xxxxxxxxxxxxx',col4:'xxxxxxxxxxxxx',col5:'xxxxxxxxxxxxx',col6:'75000'},
+            {col1:'7',col2:'xxxxxxxxxxxxx',col3:'xxxxxxxxxxxxx',col4:'xxxxxxxxxxxxx',col5:'xxxxxxxxxxxxx',col6:'100000'},
+            {col1:'8',col2:'xxxxxxxxxxxxx',col3:'xxxxxxxxxxxxx',col4:'xxxxxxxxxxxxx',col5:'xxxxxxxxxxxxx',col6:'75000'},
+            {col1:'9',col2:'xxxxxxxxxxxxx',col3:'xxxxxxxxxxxxx',col4:'xxxxxxxxxxxxx',col5:'xxxxxxxxxxxxx',col6:'200000'},
+            {col1:'10',col2:'xxxxxxxxxxxxx',col3:'xxxxxxxxxxxxx',col4:'xxxxxxxxxxxxx',col5:'xxxxxxxxxxxxx',col6:'100000'},
+            // {col1:'11',col2:'xxxxxxxxxxxxx',col3:'xxxxxxxxxxxxx',col4:'xxxxxxxxxxxxx',col5:'xxxxxxxxxxxxx',col6:'200000'},
+            // {col1:'12',col2:'xxxxxxxxxxxxx',col3:'xxxxxxxxxxxxx',col4:'xxxxxxxxxxxxx',col5:'xxxxxxxxxxxxx',col6:'50000'},
+            // {col1:'13',col2:'xxxxxxxxxxxxx',col3:'xxxxxxxxxxxxx',col4:'xxxxxxxxxxxxx',col5:'xxxxxxxxxxxxx',col6:'200000'},
+            // {col1:'14',col2:'xxxxxxxxxxxxx',col3:'xxxxxxxxxxxxx',col4:'xxxxxxxxxxxxx',col5:'xxxxxxxxxxxxx',col6:'35000'},
+            // {col1:'15',col2:'xxxxxxxxxxxxx',col3:'xxxxxxxxxxxxx',col4:'xxxxxxxxxxxxx',col5:'xxxxxxxxxxxxx',col6:'50000'},
+            // {col1:'16',col2:'xxxxxxxxxxxxx',col3:'xxxxxxxxxxxxx',col4:'xxxxxxxxxxxxx',col5:'xxxxxxxxxxxxx',col6:'100000'},
+        ];
+    })();
 
 });
+
+ 
